52cb9a4230dce68189a5da6b1597ac2600d9cf30
Lluis Gili <lluis@ingent.net>
2010-12-17 14:47:33 +0100

    add current invoice to payments select

--- app/helpers/payments_helper.rb
+++ app/helpers/payments_helper.rb
@@ -1,8 +1,12 @@
 module PaymentsHelper
 
   def invoices_for_select
-    InvoiceDocument.find(:all, :order => 'number DESC', :include => 'client', :conditions => ["clients.project_id = ? and status < ? ", @project, Invoice::STATUS_CLOSED]).collect {|c| [ "#{c.number} #{c.total.to_s.rjust(10).gsub(' ','_')}€ #{c.client}", c.id ] }
-  
+    if @payment.invoice
+      conditions = ["(clients.project_id = ? and status < ?) OR invoices.id=?", @project, Invoice::STATUS_CLOSED, @payment.invoice]
+    else
+      conditions = ["clients.project_id = ? and status < ?", @project, Invoice::STATUS_CLOSED]
+    end
+    InvoiceDocument.find(:all, :order => 'number DESC', :include => 'client', :conditions => conditions).collect {|c| [ "#{c.number} #{c.total.to_s.rjust(10).gsub(' ','_')}€ #{c.client}", c.id ] }
   end
 
 end
