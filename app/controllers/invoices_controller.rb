f7a3e4423323648a5e5ebc00e259c28704658cc4
Lluis Gili <lluis@ingent.net>
2011-01-03 11:06:29 +0100

    capture exceptions on b2b_message creation

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -173,10 +173,16 @@ class InvoicesController < ApplicationController
         i+=1
       end
       @invoice.md5=`md5sum '#{xml_file.path}'`.split.first
-      B2bMessage.connect(Setting.plugin_haltr["trace_url"])
-      channel=Setting.plugin_haltr["folder#{params[:folder]}"].split("/").last
-      b2bm = B2bMessage.new(:md5=>@invoice.md5,:name=>File.basename(destination),:b2b_channel_id=>channel)
-      b2bm.save
+      begin
+        # Insert invoice into b2brouter messages database
+        B2bMessage.connect(Setting.plugin_haltr["trace_url"])
+        channel=Setting.plugin_haltr["folder#{params[:folder]}"].split("/").last
+        b2bm = B2bMessage.new(:md5=>@invoice.md5,:name=>File.basename(destination),:b2b_channel_id=>channel)
+        b2bm.save
+      rescue Exception => e
+        #TODO
+        logger.error(e.message)
+      end
       FileUtils.mv(xml_file.path,destination)
       @invoice.queue
       flash[:info] = 'Invoice sent to the send queue'
