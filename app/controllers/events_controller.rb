ec521f3a5abea1e95a1d37ebea58023a44924f64
Lluis Gili <lluis@ingent.net>
2011-01-21 12:19:24 +0100

    fix rest for events and check remote ip

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,12 @@
 class EventsController < ApplicationController
   unloadable
 
-  before_filter :find_invoice
+  skip_before_filter :check_if_login_required, :only => [ :create ]
+  before_filter :check_remote_ip, :find_invoice
 
   def create
     @event = Event.new(params[:event])
+    @event.invoice = @invoice
     respond_to do |format|
       if @event.save
         flash[:notice] = 'Event was successfully created.'
@@ -17,6 +19,14 @@ class EventsController < ApplicationController
 
 
   private
+
+  def check_remote_ip
+    unless request.remote_ip == '127.0.0.1' #TODO: or Setting.plugin_haltr['b2brouter_ip'].gsub(/ /,'').split(",").include?(request.remote_ip)
+      render :text => "Not allowed from your IP #{request.remote_ip}\n", :status => 403
+      logger.error "Not allowed from IP #{request.remote_ip}\n" #TODO: (must be from #{Setting.plugin_haltr['b2brouter_ip']}).\n"
+      return false
+    end
+  end
 
   def find_invoice
     @invoice = InvoiceDocument.find params[:invoice_id]
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
   map.connect '/tasks/report/:id/:months_ago', :controller => 'tasks', :action => 'report'
   map.resources :invoices, :has_many => :events
+  map.resources :events
 end
