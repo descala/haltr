809057d9e02fc86926a8bf5e1500d0fcc66f9d5a
Lluis Gili <lluis@ingent.net>
2011-01-27 11:29:05 +0100

    change invoice filename to include cif and invoice.id

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -177,10 +177,10 @@ class InvoicesController < ApplicationController
     xml_file=Tempfile.new("invoice_#{@invoice.id}.xml","tmp")
     xml_file.write(render_to_string(:template => "invoices/facturae32.xml.erb", :layout => false))
     xml_file.close
-    destination="#{path}/" + "#{@project.identifier}_#{@invoice.number}.xml".gsub(/\//,'')
+    destination="#{path}/" + "#{@company.taxcode}_#{@invoice.id}.xml".gsub(/\//,'')
     i=2
     while File.exists? destination
-      destination="#{path}/" + "#{@project.identifier}_#{@invoice.number}_#{i}.xml".gsub(/\//,'')
+      destination="#{path}/" + "#{@company.taxcode}_#{i}_#{@invoice.id}.xml".gsub(/\//,'')
       i+=1
     end
     FileUtils.mv(xml_file.path,destination)
