1297bf0845a08130952e6c59e459d0220f46be3b
David Escala <descala@ingent.net>
2011-01-31 19:33:24 +0100

    Terms i18n. Fixes #2489

--- app/models/terms.rb
+++ app/models/terms.rb
@@ -4,20 +4,14 @@ class Terms
 
   KEYS = %w(0 30 60 90 120 1m1 2m1 1m15 1m20)
 
-  NOW = "al comptat"
-  DAYS = "%d dies"
-  DAYNM = "dia %d mes següent"
-  
   attr_reader :description, :due_date
   
   def initialize(code, date=Date.today)
     if code.to_i.to_s == code.to_s or code.nil? or code.blank?
       # It's a number
       if code.to_i == 0
-        @description = NOW
         @due_date = date
       else
-        @description = sprintf(DAYS,code.to_i)
         @due_date = date + code.to_i.day
       end
     else
@@ -26,15 +20,10 @@ class Terms
         months_to_add = $1.to_i
         day = $2.to_i
         date_with_months = date + months_to_add.months
-        if months_to_add == 1
-          @description = sprintf(DAYNM,day)
-        else
-          months = I18n.backend.translate("#{I18n.locale}.date.month_names", "")
-          @description = "#{day} de #{months[date_with_months.month]}"
-        end
         @due_date = Date.new(date_with_months.year,date_with_months.month,day)
       end
     end
+    @description = I18n.t(code)
   end
 
   def self.for_select
