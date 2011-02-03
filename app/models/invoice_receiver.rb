0eb09a04e82c1d31bdeeafbc11c76ab77ce4907e
David Escala <descala@ingent.net>
2011-02-03 21:37:42 +0100

    Get legal invoice needs oringal_md5

--- app/models/invoice_receiver.rb
+++ app/models/invoice_receiver.rb
@@ -19,8 +19,8 @@ class InvoiceReceiver < ActionMailer::Base
         InvoiceReceiver.log "Bounced invoice #{name} with id #{id} does not exist on haltr"
         return
       end
-      if haltr_invoice.md5 != md5
-        InvoiceReceiver.log "Bounced invoice #{name} with id #{id} does not match MD5 stored on haltr (received: #{md5} stored: #{haltr_invoice.md5})"
+      if haltr_invoice.final_md5 != md5
+        InvoiceReceiver.log "Bounced invoice #{name} with id #{id} does not match MD5 stored on haltr (received: #{md5} stored: #{haltr_invoice.final_md5})"
         return
       end
       Event.create(:name=>'bounced',:invoice=>haltr_invoice)
