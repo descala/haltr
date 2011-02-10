ce35722768a95904d260fab16688af892b7cd2f6
Lluis Gili <lluis@ingent.net>
2011-02-10 15:07:30 +0100

    don't crash on missing client

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -256,7 +256,7 @@ class InvoicesController < ApplicationController
   def find_invoice
     @invoice = InvoiceDocument.find params[:id]
     @lines = @invoice.invoice_lines
-    @client = @invoice.client || Client.new(:project=>@invoice.project)
+    @client = @invoice.client || Client.new(:name=>"unknown",:contrycode=>"ESP",:project=>@invoice.project)
     @project = @invoice.project
   rescue ActiveRecord::RecordNotFound
     render_404
