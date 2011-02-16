34ad3a2e24a7fa089d45d66000702fe0c4a44983
David Escala <descala@ingent.net>
2011-02-16 13:05:12 +0100

    Fix quantity with round

--- db/migrate/20110216110612_use_decimal_on_invoice_lines.rb
+++ db/migrate/20110216110612_use_decimal_on_invoice_lines.rb
@@ -1,6 +1,7 @@
 class UseDecimalOnInvoiceLines < ActiveRecord::Migration
   def self.up
     # InvoiceLine.all.collect {|i| i.price = (i.price/100).to_s ; i.save(false) }
+    # InvoiceLine.all.collect {|i| i.quantity = i.quantity.round(2).to_s ; i.save(false) }
     change_column :invoice_lines, :quantity, :decimal, :precision => 18, :scale => 9
     change_column :invoice_lines, :price_in_cents, :decimal, :precision => 18, :scale => 9
     rename_column :invoice_lines, :price_in_cents, :price
