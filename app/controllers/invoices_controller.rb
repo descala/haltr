f4e385d1320b373930695aa5cc27c1acd52ddbf6
Lluis Gili <lluis@ingent.net>
2011-06-10 10:08:11 +0200

    render_404 if can't find client by hash

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -300,7 +300,6 @@ class InvoicesController < ApplicationController
 
   def view
     @lines = @invoice.invoice_lines
-    @client = @invoice.client || Client.new(:name=>"unknown",:country=>"ES",:taxcode=>"EUR",:project=>@invoice.project)
     render :layout=>"public"
   rescue
     render_404
@@ -340,6 +339,10 @@ class InvoicesController < ApplicationController
   def find_hashid
     Project.send(:include, ProjectHaltrPatch) #TODO: perque nomes funciona el primer cop sense aixo?
     @client = Client.find_by_hashid params[:id]
+    if @client.nil?
+      render_404
+      return
+    end
     @company = @client.project.company
     invoices = IssuedInvoice.find(:all, :conditions => ["client_id=? AND id=?",@client.id,params[:invoice_id]])
     if invoices.size != 1
