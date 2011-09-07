f14bc08f79a3748c93ad85c9992ce448c0163e33
Lluis Gili <lluis@ingent.net>
2011-09-07 13:49:36 +0200

    afegir codi a la descripcio dels centres cestors de la diba, refs #2913

--- app/models/diba.rb
+++ app/models/diba.rb
@@ -2,96 +2,96 @@ class Diba
 
   CODIS_CENTRE_GESTORS = {
     "Àrea Presidència" => [
-      ["Secretaria General", "01000"],
-      ["Direcció de Relacions Internacionals", "10400"],
-      ["Direcció de Comunicació", "10600"],
-      ["Gerència", "11000"],
-      ["SubDirecció d'Organització, Processos i Informació Corporativa", "11010"],
-      ["Direcció de Serveis de Formació", "11100"],
-      ["Coordinació de Concertació i Assistència Local", "11C00"],
-      ["Servei de Govern Local", "11C01"],
-      ["Gerencia de Serveis d'Assistència al Govern Local", "11C10"]
+      ["Secretaria General (01000)", "01000"],
+      ["Direcció de Relacions Internacionals (10400)", "10400"],
+      ["Direcció de Comunicació (10600)", "10600"],
+      ["Gerència (11000)", "11000"],
+      ["SubDirecció d'Organització, Processos i Informació Corporativa (11010)", "11010"],
+      ["Direcció de Serveis de Formació (11100)", "11100"],
+      ["Coordinació de Concertació i Assistència Local (11C00)", "11C00"],
+      ["Servei de Govern Local (11C01)", "11C01"],
+      ["Gerencia de Serveis d'Assistència al Govern Local (11C10)", "11C10"]
     ],
     "Àrea d'Hisenda i Recursos Interns" => [
-      ["Direcció de Serveis de Planificació Econòmica", "20100"],
-      ["Servei de Programació", "20101"],
-      ["Oficina de Patrimoni i Gestió immobiliària", "20102"],
-      ["Intervenció General", "21000"],
-      ["Tresoreria", "23000"],
-      ["Coordinació d'Hisenda i Recursos Interns", "24000"],
-      ["Servei de Contractació", "24001"],
-      ["Direcció dels Serveis de Recursos Humans", "24100"],
-      ["SubDirecció de Logística", "24210"],
-      ["SubDirecció d'Edificació", "24220"],
-      ["Direcció de Serveis de Tecnologies i Sistemes Corporatius", "24300"]
+      ["Direcció de Serveis de Planificació Econòmica (20100)", "20100"],
+      ["Servei de Programació (20101)", "20101"],
+      ["Oficina de Patrimoni i Gestió immobiliària (20102)", "20102"],
+      ["Intervenció General (21000)", "21000"],
+      ["Tresoreria (23000)", "23000"],
+      ["Coordinació d'Hisenda i Recursos Interns (24000)", "24000"],
+      ["Servei de Contractació (24001)", "24001"],
+      ["Direcció dels Serveis de Recursos Humans (24100)", "24100"],
+      ["SubDirecció de Logística (24210)", "24210"],
+      ["SubDirecció d'Edificació (24220)", "24220"],
+      ["Direcció de Serveis de Tecnologies i Sistemes Corporatius (24300)", "24300"]
     ],
     "Àrea d'Infraestructures, Urbanisme i Habitatge" => [
-      ["Coordinació de l'Àrea d'Infraestructures, Urbanisme i Habitage", "31000"],
-      ["Oficina Tècnica de Cartografia i SIG Local", "31001"],
-      ["Gerència de Serveis d'Infraestructures Viàries i Mobilitat", "31100"],
-      ["Gerència de Serveis d'Equipaments, Infraestructures Urbanes i Patrimoni Arquitectònic", "31200"],
-      ["Servei de Patrimoni Arquitectònic Local", "31201"],
-      ["Servei d'Equipaments i Espai Públic", "31202"],
-      ["Gerència de Serveis d'Habitatge, Urbanisme i Activitats", "31300"]
+      ["Coordinació de l'Àrea d'Infraestructures, Urbanisme i Habitage (31000)", "31000"],
+      ["Oficina Tècnica de Cartografia i SIG Local (31001)", "31001"],
+      ["Gerència de Serveis d'Infraestructures Viàries i Mobilitat (31100)", "31100"],
+      ["Gerència de Serveis d'Equipaments, Infraestructures Urbanes i Patrimoni Arquitectònic (31200)", "31200"],
+      ["Servei de Patrimoni Arquitectònic Local (31201)", "31201"],
+      ["Servei d'Equipaments i Espai Públic (31202)", "31202"],
+      ["Gerència de Serveis d'Habitatge, Urbanisme i Activitats (31300)", "31300"]
     ],
     "Àrea d'Esports" => [
-      ["Gerència de Serveis d'Esports", "41100"]
+      ["Gerència de Serveis d'Esports (41100)", "41100"]
     ],
     "Àrea de Salut Pública i Consum" => [
-      ["Coordinació de l'Área de Salut Pública", "51000"],
-      ["Servei de Salut Pública", "51101"],
-      ["Servei de Suport a les Polítiques de Consum", "51102"]
+      ["Coordinació de l'Área de Salut Pública (51000)", "51000"],
+      ["Servei de Salut Pública (51101)", "51101"],
+      ["Servei de Suport a les Polítiques de Consum (51102)", "51102"]
     ],
     "Àrea de Benestar Social" => [
-      ["Coordinació de l'Àrea de Benestar Social", "61000"],
-      ["Gerència de Serveis de Benestar Social", "61100"],
-      ["Servei d'Acció Social", "61101"],
-      ["Oficina de Suport Tècnic als Serveis Socials", "61104"],
-      ["Oficina de Suport Tècnic a l'Autonomia Personal i Atenció a la Dependència", "61105"],
-      ["Gerència de Serveis Residencials d'Estades Temporals i Respir", "61200"],
-      ["Oficina de Suport Tècnic i Logístic", "61202"]
+      ["Coordinació de l'Àrea de Benestar Social (61000)", "61000"],
+      ["Gerència de Serveis de Benestar Social (61100)", "61100"],
+      ["Servei d'Acció Social (61101)", "61101"],
+      ["Oficina de Suport Tècnic als Serveis Socials (61104)", "61104"],
+      ["Oficina de Suport Tècnic a l'Autonomia Personal i Atenció a la Dependència (61105)", "61105"],
+      ["Gerència de Serveis Residencials d'Estades Temporals i Respir (61200)", "61200"],
+      ["Oficina de Suport Tècnic i Logístic (61202)", "61202"]
     ],
     "Àrea de Cultura" => [
-      ["Coordinació de l'Àrea de Cultura", "71000"],
-      ["Oficina d'Estudis i Recursos Culturals", "71101"],
-      ["Oficina Patrimoni Cultural", "71102"],
-      ["Oficina de Difusió Artística", "71103"],
-      ["Gerència de Serveis de Biblioteques", "71200"]
+      ["Coordinació de l'Àrea de Cultura (71000)", "71000"],
+      ["Oficina d'Estudis i Recursos Culturals (71101)", "71101"],
+      ["Oficina Patrimoni Cultural (71102)", "71102"],
+      ["Oficina de Difusió Artística (71103)", "71103"],
+      ["Gerència de Serveis de Biblioteques (71200)", "71200"]
     ],
     "Àrea d'Educació" => [
-      ["Gerència de Serveis d'Educació", "81100"]
+      ["Gerència de Serveis d'Educació (81100)", "81100"]
     ],
     "Àrea d'Espais Naturals" => [
-      ["Coordinació de l'Àrea d'Espais Naturals", "A1000"],
-      ["Oficina Tècnica de Prevenció Municiapal d'Incendis Forestals", "A1001"],
-      ["Gerència de Serveis d'Espais Naturals", "A1100"],
-      ["Oficina Tècnica de Planificació i Anàlisi Territorial", "A1101"],
-      ["Oficina Tècnica de Parcs Naturals", "A1102"],
-      ["Oficina Tècnica d'Acció Territorial", "A1103"]
+      ["Coordinació de l'Àrea d'Espais Naturals (A1000)", "A1000"],
+      ["Oficina Tècnica de Prevenció Municiapal d'Incendis Forestals (A1001)", "A1001"],
+      ["Gerència de Serveis d'Espais Naturals (A1100)", "A1100"],
+      ["Oficina Tècnica de Planificació i Anàlisi Territorial (A1101)", "A1101"],
+      ["Oficina Tècnica de Parcs Naturals (A1102)", "A1102"],
+      ["Oficina Tècnica d'Acció Territorial (A1103)", "A1103"]
     ],
     "Àrea de Medi Ambient" => [
-      ["Gerència de Serveis de Medi Ambient", "B1100"]
+      ["Gerència de Serveis de Medi Ambient (B1100)", "B1100"]
     ],
     "Àrea de Desenvolupament Econòmic" => [
-      ["Gerència de Serveis de Desenvolupament Econòmic", "C1100"],
-      ["Servei de Mercat de Treball", "C1101"],
-      ["Oficina Tècnica d'Estratègies per al Desenvolupament Econòmic", "C1102"],
-      ["Servei de Teixit Productiu", "C1103"],
-      ["Delegació de Turisme", "C1A00"],
-      ["Oficina Tècnica de Turisme", "C1A01"],
-      ["Oficina de Promoció Turística", "C1A02"]
+      ["Gerència de Serveis de Desenvolupament Econòmic (C1100)", "C1100"],
+      ["Servei de Mercat de Treball (C1101)", "C1101"],
+      ["Oficina Tècnica d'Estratègies per al Desenvolupament Econòmic (C1102)", "C1102"],
+      ["Servei de Teixit Productiu (C1103)", "C1103"],
+      ["Delegació de Turisme (C1A00)", "C1A00"],
+      ["Oficina Tècnica de Turisme (C1A01)", "C1A01"],
+      ["Oficina de Promoció Turística (C1A02)", "C1A02"]
     ],
     "Àrea d'Igualtat i Ciutadania" => [
-      ["Coordinació de l'Àrea d'Igualtat i Ciutadania", "D1000"],
-      ["Oficina de Promoció de Polítiques d'Igualtat Dona-Home", "D1101"],
-      ["Oficina del Pla Jove", "D1102"],
-      ["Serveis de Polítiques de Diversitat i Ciutadania", "D1103"],
-      ["Oficina de Participació Ciutadana", "D1104"]
+      ["Coordinació de l'Àrea d'Igualtat i Ciutadania (D1000)", "D1000"],
+      ["Oficina de Promoció de Polítiques d'Igualtat Dona-Home (D1101)", "D1101"],
+      ["Oficina del Pla Jove (D1102)", "D1102"],
+      ["Serveis de Polítiques de Diversitat i Ciutadania (D1103)", "D1103"],
+      ["Oficina de Participació Ciutadana (D1104)", "D1104"]
     ],
     "Àrea de Comerç" => [
-      ["Gerència de Serveis de Comerç", "E1100"],
-      ["Servei de Comerç Urbà", "E1101"],
-      ["Oficina de Mercats i Fires Locals", "E1102"]
+      ["Gerència de Serveis de Comerç (E1100)", "E1100"],
+      ["Servei de Comerç Urbà (E1101)", "E1101"],
+      ["Oficina de Mercats i Fires Locals (E1102)", "E1102"]
     ]
   }
 
