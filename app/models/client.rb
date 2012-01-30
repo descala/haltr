dcf3a1de2ddf154ef9efebbc9839564629907f0f
Lluis Gili <lluis@ingent.net>
2012-01-30 11:42:53 +0100

    validate clients email address, if any

--- app/models/client.rb
+++ app/models/client.rb
@@ -18,6 +18,7 @@ class Client < ActiveRecord::Base
   validates_inclusion_of    :currency, :in  => Money::Currency::TABLE.collect {|k,v| v[:iso_code] }, :if => Proc.new {|c| c.company_id.blank? }
   validates_numericality_of :bank_account, :unless => Proc.new { |c| c.bank_account.blank? }
   validates_length_of       :bank_account, :within => 16..40, :unless => Proc.new { |c| c.bank_account.blank? }
+  validates_format_of       :email, :with => /\A([\w\.\-\+]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, :allow_nil => true
 #  validates_uniqueness_of :name, :scope => :project_id
 #  validates_length_of :name, :maximum => 30
 #  validates_format_of :identifier, :with => /^[a-z0-9\-]*$/
