280d1d886523179152b76bbfe36e1f7020ea1585
Lluis Gili <lluis@ingent.net>
2012-01-30 09:40:53 +0100

    remove postalcode length validation, since it is contry dependant. closes #25

--- app/models/company.rb
+++ app/models/company.rb
@@ -8,7 +8,6 @@ class Company < ActiveRecord::Base
   validates_presence_of :name, :project_id, :email
   validates_length_of :taxcode, :maximum => 20
   validates_uniqueness_of :taxcode
-  validates_length_of :postalcode, :is => 5
   validates_numericality_of :bank_account, :allow_nil => true, :unless => Proc.new {|company| company.bank_account.blank?}
   validates_length_of :bank_account, :maximum => 20
   validates_inclusion_of :currency, :in  => Money::Currency::TABLE.collect {|k,v| v[:iso_code] }
