11deb3fce519f61426a85fff036835ffb3060fa1
Lluis Gili <lluis@ingent.net>
2013-02-11 10:39:55 +0100

    fix add taxes link

--- app/helpers/companies_helper.rb
+++ app/helpers/companies_helper.rb
@@ -4,7 +4,7 @@ module CompaniesHelper
     link_to_function l(:button_add_tax), :class=>"icon icon-add" do |page|
       company_form.fields_for(:taxes, Tax.new, :child_index => 'NEW_RECORD') do |tax_form|
         html = render(:partial => 'companies/tax', :locals => { :f => tax_form })
-        page << "$('taxes').insert({ bottom: '#{escape_javascript(html)}'.replace(/NEW_RECORD/g, new Date().getTime()) });"
+        page << "$('taxes_table').insert({ bottom: '#{escape_javascript(html)}'.replace(/NEW_RECORD/g, new Date().getTime()) });"
       end
     end
   end
