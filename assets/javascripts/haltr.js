119b53c029b48e0e44011ed12471b26e2596fb2e
Lluis Gili <lluis@ingent.net>
2013-05-10 15:12:15 +0200

    invoices form

--- assets/javascripts/haltr.js
+++ assets/javascripts/haltr.js
@@ -1,9 +1,9 @@
 /* Show/hide payment method textarea */
 function payment_method_changed(obj_name) {
-  if ($(obj_name+'_payment_method').value == 13) {
-    $(obj_name+'_payment_method_text').removeClassName('hidden');
+  if ($('#'+obj_name+'_payment_method').value == 13) {
+    $('#'+obj_name+'_payment_method_text').removeClassName('hidden');
   } else {
-    $(obj_name+'_payment_method_text').addClassName('hidden');
+    $('#'+obj_name+'_payment_method_text').addClassName('hidden');
   }
 }
 
