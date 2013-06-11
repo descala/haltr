5a11bfc0b25dd409b1145a1b33af4bb42aa7c1b1
David Escala <descala@ingent.net>
2013-06-11 09:52:37 +0200

    recover my_company link. styles

--- lib/haltr.rb
+++ lib/haltr.rb
@@ -11,7 +11,7 @@ require 'haltr/menu_item'
 
 Redmine::MenuManager.map :companies_menu do |menu|
   menu.push :companies_level2, {:controller=>'clients', :action => 'index' }, :param => :project_id, :caption => :label_companies
-  menu.push :my_company, {:controller=>'companies', :action => 'index' }, :param => :project_id
+  menu.push :my_company, {:controller=>'companies', :action => 'my_company' }, :param => :project_id
   menu.push :linked_to_mine, {:controller=>'companies', :action => 'linked_to_mine' }, :param => :project_id, :if => Proc.new { |p| Client.all(:conditions => ['company_id = ?', p.company]).any? }
   menu.push :people, {:controller=>'people', :action => 'index' }, :param => :project_id
 end
