a443e77e5403e30093e2a67cc551548d540c2fdf
Lluis Gili <lluis@ingent.net>
2013-06-17 09:53:06 +0200

    translate people menu entry

--- init.rb
+++ init.rb
@@ -87,6 +87,7 @@ Redmine::Plugin.register :haltr do
   menu :project_menu, :companies,  { :controller => 'clients',  :action => 'index' }, :param => :project_id, :caption => :label_companies
   menu :project_menu, :invoices,   { :controller => 'invoices', :action => 'index' }, :param => :project_id, :caption => :label_invoice_plural
   menu :project_menu, :payments,   { :controller => 'payments', :action => 'index' }, :param => :project_id, :caption => :label_payment_plural
+  # submenus defined at lib/haltr.rb
 
 end
 
