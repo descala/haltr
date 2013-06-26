e11ca3dda6efbc180fc0dc8be489483df25a61f6
Lluis Gili <lluis@ingent.net>
2013-06-26 15:13:12 +0200

    grouped invoice list on payments

--- app/helpers/payments_helper.rb
+++ app/helpers/payments_helper.rb
@@ -7,9 +7,12 @@ module PaymentsHelper
     else
       cond = ["clients.project_id = ? and state != 'closed'", @project]
     end
+    invoices = {I18n.t("IssuedInvoice")=>[],I18n.t("ReceivedInvoice")=>[]}
     InvoiceDocument.find(:all, :order => 'number DESC', :include => 'client', :conditions => cond).collect do |c|
-      [ "#{c.number} #{c.total.to_s.rjust(10).gsub(' ','_')}€ #{c.client}", c.id ]
+      #[ "#{c.class} - #{c.number} #{c.total.to_s.rjust(10).gsub(' ','_')}€ #{c.client}", c.id ]
+      invoices[I18n.t(c.type)] << [ "#{c.number} #{c.total.to_s.rjust(10).gsub(' ','_')}€ #{c.client}", c.id ]
     end
+    invoices
   end
 
   def n19_fix(string,n=40)
