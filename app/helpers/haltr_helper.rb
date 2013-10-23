47fe4c0ef4a6e5f8cc09b166d3df672a99e531f2
Lluis Gili <lluis@ingent.net>
2013-10-23 14:25:59 +0200

    add html_options to recognize_path, fixes non-get requests

--- app/helpers/haltr_helper.rb
+++ app/helpers/haltr_helper.rb
@@ -92,7 +92,7 @@ module HaltrHelper
   # (to allow use with new _path helpers)
   def link_to_if_authorized(name, options = {}, html_options = nil, *parameters_for_method_reference)
     if options.is_a?(String)
-      options = Rails.application.routes.recognize_path(options)
+      options = Rails.application.routes.recognize_path(options, html_options)
     end
     link_to(name, options, html_options, *parameters_for_method_reference) if authorize_for(options[:controller] || params[:controller], options[:action])
   rescue ActionController::RoutingError
