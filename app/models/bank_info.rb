afc6b2dc765bcd6f1245dade38e2c2b8a9da5a6f
Lluis Gili <lluis@ingent.net>
2013-11-13 12:00:23 +0100

    iban requires bic, closes #4259

--- app/models/bank_info.rb
+++ app/models/bank_info.rb
@@ -7,10 +7,17 @@ class BankInfo < ActiveRecord::Base
   validates_numericality_of :bank_account, :allow_nil => true, :allow_blank => true
   validates_length_of :bank_account, :maximum => 20
   validate :has_one_account
+  validate :iban_requires_bic
 
   def has_one_account
     if [bank_account, iban, bic].compact.reject(&:blank?).empty?
       errors.add(:base, "empty values for bank_account, iban and bic")
+    end
+  end
+
+  def iban_requires_bic
+    unless iban.blank? and bic.blank?
+      errors.add(:base, "iban requires bic") if iban.blank? or bic.blank?
     end
   end
 
