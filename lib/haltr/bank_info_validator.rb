3f0494bff26a18fbe341c6b115d920ad07f8166d
Lluis Gili <lluis@ingent.net>
2014-01-29 13:32:38 +0100

    remove bank_account validation

--- lib/haltr/bank_info_validator.rb
+++ lib/haltr/bank_info_validator.rb
@@ -2,8 +2,6 @@ module Haltr::BankInfoValidator
 
   def self.included(base)
     base.class_eval do
-      validates_numericality_of :bank_account, :allow_nil => true, :allow_blank => true
-      validates_length_of :bank_account, :maximum => 20, :allow_nil => true, :allow_blank => true
       validates_length_of :bic, :in => 8..11, :allow_nil => true, :allow_blank => true
 
       validate :check_iban_is_ok
