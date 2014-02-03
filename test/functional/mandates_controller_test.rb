699ce1974dae453cd0e6d50df3e4827cc3bc007b
Lluis Gili <lluis@ingent.net>
2014-02-03 11:10:07 +0100

    test skel for mandates controller

---
+++ test/functional/mandates_controller_test.rb
@@ -0,0 +1,14 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+class MandatesControllerTest < ActionController::TestCase
+
+  fixtures :mandates
+
+  def setup
+  end
+
+  def test_true
+    assert true
+  end
+
+end
