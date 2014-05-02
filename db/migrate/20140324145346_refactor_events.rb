59678e719e250bc8da31dad47e1ef47e2efabb7b
Lluís <lluis@ingent.net>
2014-05-02 14:03:09 +0200

    fix migration for pg

--- db/migrate/20140324145346_refactor_events.rb
+++ db/migrate/20140324145346_refactor_events.rb
@@ -4,16 +4,16 @@ class RefactorEvents < ActiveRecord::Migration
     add_column :events, :file, :text, :limit => 16777215
     add_column :events, :content_type, :string
 
-    execute 'UPDATE events SET type="ReceivedInvoiceEvent" WHERE name="email";'
-    execute 'UPDATE events SET type="EventWithUrl" WHERE md5 is not NULL;'
-    execute 'UPDATE events SET type="EventWithMail" WHERE name LIKE "%paid_notification" and md5 is NULL;'
+    execute "UPDATE events SET type='ReceivedInvoiceEvent' WHERE name='email';"
+    execute "UPDATE events SET type='EventWithUrl' WHERE md5 is not NULL;"
+    execute "UPDATE events SET type='EventWithMail' WHERE name LIKE '%paid_notification' and md5 is NULL;"
 
     # info is now a serialized Hash
-    execute 'UPDATE events SET info=concat("---\n' +
-      ':notes: ", COALESCE(info,""), "\n' +
-      ':md5: ", COALESCE(md5,""), "\n' +
-      ':final_md5: ", COALESCE(final_md5,""), "\n' +
-      '");'
+    execute "UPDATE events SET info=concat('---\n" +
+      ":notes: ', COALESCE(info,''), '\n" +
+      ":md5: ', COALESCE(md5,''), '\n" +
+      ":final_md5: ', COALESCE(final_md5,''), '\n" +
+      "');"
 
     remove_column :events, :md5
     remove_column :events, :final_md5
