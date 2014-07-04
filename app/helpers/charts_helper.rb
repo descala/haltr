7e22fde4ef2b2fc41b1105ec151c15c2239df92d
Lluis Gili <lluis@ingent.net>
2014-07-04 12:38:52 +0200

    allow admin to use my/page of other projects

--- app/helpers/charts_helper.rb
+++ app/helpers/charts_helper.rb
@@ -3,13 +3,17 @@ module ChartsHelper
   def haltr_projects
     user = User.current
     projs = []
-    user.projects.each do |project|
-      if project.module_enabled? :haltr and user.allowed_to?(:general_use, project) and project.company
-        if projs.size >= 5
-          flash.now[:error] = "Too many projects to display (showing 5)"
-          break
+    if user.admin? and @project
+      projs << @project
+    else
+      user.projects.each do |project|
+        if project.module_enabled? :haltr and user.allowed_to?(:general_use, project) and project.company
+          if projs.size >= 5
+            flash.now[:error] = "Too many projects to display (showing 5)"
+            break
+          end
+          projs << project
         end
-        projs << project
       end
     end
     projs
