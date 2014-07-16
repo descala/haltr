2d230217f0dafbad46b703238050370d2eb1c302
Lluis Gili <lluis@ingent.net>
2014-07-16 15:03:01 +0200

    select prefs for invoice_status chart, with bug: updates other chart, refs #3775

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -37,7 +37,21 @@ class ChartsController < ApplicationController
   end
 
   def invoice_status
-    chart_data = @project.issued_invoices.where(["date > ?", 5.years.ago]).group(:state).group_by_month(:date, format: "%Y/%m").sum('total_in_cents/100')
+    case User.current.pref.others[:chart_invoice_status]
+    when "all_by_year"
+      projdata = @project.issued_invoices.where(["date > ?", 7.years.ago]).
+        group(:state).group_by_year(:date, format: "%Y/%m").sum('total_in_cents/100')
+    when "last_year_by_month"
+      projdata = @project.issued_invoices.where(["date > ?", 1.years.ago]).
+        group(:state).group_by_month(:date, format: "%Y/%m").sum('total_in_cents/100')
+    when "last_month_by_week"
+      projdata = @project.issued_invoices.where(["date > ?", 1.month.ago]).
+        group(:state).group_by_week(:date, format: "%Y/%m").sum('total_in_cents/100')
+    else # all_by_month
+      projdata = @project.issued_invoices.where(["date > ?", 7.years.ago]).
+        group(:state).group_by_month(:date, format: "%Y/%m").sum('total_in_cents/100')
+    end
+    chart_data = projdata
     final_json = JSON.parse(chart_data.chart_json)
     final_json.each do |h|
       state_key = h['name']
