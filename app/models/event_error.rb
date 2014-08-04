1d386128a63315d0a32882b9883b39ef2104c207
Lluis Gili <lluis@ingent.net>
2014-08-04 11:11:15 +0200

    join notes if it is an array

--- app/models/event_error.rb
+++ app/models/event_error.rb
@@ -1,7 +1,14 @@
 class EventError < Event
   def to_s
     str = l(name)
-    str += ": #{I18n.t(notes, :default => notes)}" unless notes.blank?
+    unless notes.blank?
+      str += ": "
+      if notes.is_an?(Array)
+        str += I18n.t(notes, :default => notes).join(" ")
+      else
+        str += I18n.t(notes, :default => notes)
+      end
+    end
     str
   end
 end
