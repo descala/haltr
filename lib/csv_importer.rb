79700d3e765c930a116b9ee09b2f0887636de8cf
Lluis Gili <lluis@ingent.net>
2014-12-01 10:48:56 +0100

    set external_company default invoice_format

--- lib/csv_importer.rb
+++ lib/csv_importer.rb
@@ -159,7 +159,7 @@ module CsvImporter
     external_companies.each do |ec|
       ec_hash = ec.members.inject({}) {|h,m|
         h[m] = ec[m]; h.reverse_merge(
-          {country: 'es', currency: 'EUR'}
+          {country: 'es', currency: 'EUR', invoice_format: 'aoc32'}
         )
       }
       current = ExternalCompany.find_by_taxcode(ec.taxcode)
