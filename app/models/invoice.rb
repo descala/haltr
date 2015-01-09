f4f8cf0b64f7d93ee7c3abd11f5416756c229f9e
Lluis Gili <lluis@ingent.net>
2015-01-09 12:51:30 +0100

    fix discount_percent bug

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -250,7 +250,7 @@ class Invoice < ActiveRecord::Base
   end
 
   def discount_amount(tax_type=nil)
-    discount_percent = 0 if self.discount_percent.nil?
+    self.discount_percent = 0 if self.discount_percent.nil?
     gross_subtotal * (discount_percent / 100.0)
   end
 
