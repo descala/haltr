f5343a21dcbf5c99ca8cfeefda33b1e55af87297
Lluis Gili <lluis@ingent.net>
2015-04-17 12:33:16 +0200

    logs for bulksender

--- lib/haltr/bulk_sender.rb
+++ lib/haltr/bulk_sender.rb
@@ -10,14 +10,17 @@ module Haltr
 
     def perform
       invoices.each do |invoice|
+        Rails.logger.info("[BulkSender] TRYING invoice  #{invoice.id}")
         begin
           Haltr::Sender.send_invoice(invoice, user)
           invoice.queue || invoice.requeue
+          Rails.logger.info("[BulkSender] SENT   invoice  #{invoice.id}")
         rescue Exception => error
           HiddenEvent.create(:name      => "error",
                              :invoice   => invoice,
                              :error     => error.message,
                              :backtrace => error.backtrace)
+          Rails.logger.info("[BulkSender] ERROR  invoice  #{invoice.id} (#{error})")
         end
       end
     end
