cc497a282be9e7b91b205ea9abb5d6e9cda21ee6
Lluis Gili <lluis@ingent.net>
2015-05-26 09:56:22 +0200

    fix error message when rmagick is not installed

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -107,7 +107,9 @@ class CompaniesController < ApplicationController
               image = Magick::Image.read("#{attachment.diskfile}").first
               image.change_geometry!('350x130>') {|cols,rows,img| img.resize!(cols, rows)}
               image.write("#{attachment.diskfile}")
-            rescue LoadError, Magick::ImageMagickError
+            rescue LoadError
+              flash[:warning] = $!.message
+            rescue Magick::ImageMagickError
               flash[:warning] = l(:logo_not_image)
             end
           else
