5fab1ceea38d9a73048dd185daf12b0755010d1a
Lluis Gili <lluis@ingent.net>
2015-09-15 14:08:02 +0200

    change default for round_before_sum

--- db/migrate/20150915102441_add_rounding_method_to_companies.rb
+++ db/migrate/20150915102441_add_rounding_method_to_companies.rb
@@ -2,7 +2,7 @@ class AddRoundingMethodToCompanies < ActiveRecord::Migration
 
   def up
     add_column :companies, :rounding_method, :string, default: :half_up
-    add_column :companies, :round_before_sum, :boolean, default: true
+    add_column :companies, :round_before_sum, :boolean, default: false
   end
 
   def down
