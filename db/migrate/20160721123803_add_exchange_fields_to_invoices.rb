84fb7def7dae2bc90d3ab68abebab7001826d5e6
Lluís Gili <lluis@ingent.net>
2016-07-22 09:43:41 +0200

    don't update already created invoices

--- db/migrate/20160721123803_add_exchange_fields_to_invoices.rb
+++ db/migrate/20160721123803_add_exchange_fields_to_invoices.rb
@@ -3,10 +3,6 @@ class AddExchangeFieldsToInvoices < ActiveRecord::Migration
   def up
     add_column :invoices, :exchange_rate, :float
     add_column :invoices, :exchange_date, :date
-    Invoice.where("currency != 'EUR'").each {|invoice|
-      invoice.update_attribute(:exchange_rate, 1)
-      invoice.update_attribute(:exchange_date, Date.today)
-    }
   end
 
   def down
