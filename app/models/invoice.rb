685fa7de26d535180839c9955a03df2d3d756cd0
Marcos <marcos.graup@gmail.com>
2016-07-22 12:59:55 +0200

    Link to external company on import refs #6093

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1175,7 +1175,9 @@ _INV
     end
     self.client   = project.clients.where('taxcode like ?', "%#{client_hash[:taxcode]}").first
     self.client ||= project.clients.where('? like concat("%", taxcode) and taxcode != ""', client_hash[:taxcode]).first
-    if client
+    external_compamy = ExternalCompany.where('taxcode like ?', "%#{client_hash[:taxcode]}").first
+    external_compamy ||= ExternalCompany.where('? like concat("%", taxcode) and taxcode != ""', client_hash[:taxcode]).first
+    if client && external_compamy.nil?
       # client found by taxcode, but stored data may not match data in invoice
       # if it doesn't, we create a client_office with data from invoice
       to_match = {
@@ -1215,8 +1217,13 @@ _INV
         end
       end
     else
-      self.client = Client.new(client_hash)
-      client.project = self.project
+      unless client
+        self.client = Client.new(client_hash)
+        client.project = self.project
+        if external_compamy
+          self.client.company = external_compamy
+        end
+      end
       client.save!(:validate=>false)
       logger.info "created new client \"#{client.name}\" with cif #{client.taxcode} for company #{project.company.name}. time=#{Time.now}"
     end
