648c9c3f3ddaed81108d041139da9ea28005b1bb
Lluís Gili <lluis@ingent.net>
2016-10-07 10:33:48 +0200

    remove edi_code_is_unique_in_project validation, refs #6187

--- app/models/client_office.rb
+++ app/models/client_office.rb
@@ -6,7 +6,6 @@ class ClientOffice < ActiveRecord::Base
   delegate :project, to: :client
   has_many :invoices, dependent: :nullify
   validates_presence_of :name, :client_id
-  validate :edi_code_is_unique_in_project
 
   iso_country :country
 
@@ -31,23 +30,6 @@ class ClientOffice < ActiveRecord::Base
 
   def country_alpha3
     country
-  end
-
-  def edi_code_is_unique_in_project
-    if edi_code.present?
-      project.client_offices.each do |co|
-        next unless co.edi_code.to_s.chomp.casecmp(edi_code.to_s.chomp) == 0
-        next if co.eql?(self)
-        errors.add(:edi_code, :taken)
-        return
-      end
-      project.clients.each do |c|
-        next unless c.edi_code.to_s.chomp.casecmp(edi_code.to_s.chomp) == 0
-        next if c.eql?(client)
-        errors.add(:edi_code, :taken)
-        return
-      end
-    end
   end
 
 end
