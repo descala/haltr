48ef6938fb78d0379898d2ba2fa66fa3deaa47a6
David Escala <descala@ingent.net>
2016-10-20 13:28:23 +0200

    Ensure tag values are always int

--- app/models/invoice_img.rb
+++ app/models/invoice_img.rb
@@ -15,28 +15,7 @@ class InvoiceImg < ActiveRecord::Base
     if data.is_a? HashWithIndifferentAccess or Rails.env == 'test'
       # if data is a HashWithIndifferentAccess tranform to a Hash
       # ensure data keys are symbols and tag numbers are integers
-      initial_data = data
-      initial_tags = initial_data['tags'] || {}
-      initial_tokens= initial_data['tokens'] || {}
-      self.data = {}
-      self.data[:tags] = {}
-      self.data[:tokens] = {}
-      self.data[:width] = initial_data['width']
-      self.data[:height] = initial_data['height']
-      self.data[:name] = initial_data['name']
-      initial_tags.each do |tag, token_number|
-        value = token_number.to_i rescue token_number
-        self.data[:tags][tag.to_sym] = value
-      end
-      initial_tokens.each do |token_number, token_data|
-        token = {}
-        token[:text] = token_data['text']
-        token[:x0] = token_data['x0'].to_i
-        token[:x1] = token_data['x1'].to_i
-        token[:y0] = token_data['y0'].to_i
-        token[:y1] = token_data['y1'].to_i
-        self.data[:tokens][token_number.to_i] = token
-      end
+      fix_datatypes
     end
   end
 
@@ -46,6 +25,39 @@ class InvoiceImg < ActiveRecord::Base
       Event.create(name: 'processed_pdf', invoice: invoice)
     else
       EventError.create(name: 'processed_pdf', invoice: invoice, notes: 'OCR failed')
+    end
+  end
+
+  def fix_datatypes
+    initial_data = data
+    initial_tags = initial_data['tags'] || {}
+    initial_tokens= initial_data['tokens'] || {}
+    self.data = {}
+    self.data[:tags] = {}
+    self.data[:tokens] = {}
+    self.data[:width] = initial_data['width']
+    self.data[:height] = initial_data['height']
+    self.data[:name] = initial_data['name']
+    initial_tags.each do |tag, token_number|
+      value = nil
+      if token_number.is_a? Array
+        value = []
+        token_number.each do |num|
+          value << num.to_i rescue token_number
+        end
+      else
+        value = token_number.to_i rescue token_number
+      end
+      self.data[:tags][tag.to_sym] = value
+    end
+    initial_tokens.each do |token_number, token_data|
+      token = {}
+      token[:text] = token_data['text']
+      token[:x0] = token_data['x0'].to_i
+      token[:x1] = token_data['x1'].to_i
+      token[:y0] = token_data['y0'].to_i
+      token[:y1] = token_data['y1'].to_i
+      self.data[:tokens][token_number.to_i] = token
     end
   end
 
@@ -132,11 +144,11 @@ class InvoiceImg < ActiveRecord::Base
     reference = tags[key]
     if reference.is_a? Array
       reference.collect do |ref|
-        tokens[ref][:text]
+        tokens[ref.to_i][:text]
       end.join(' ')
     else
-      if tokens[reference]
-        tokens[reference][:text]
+      if tokens[reference.to_i]
+        tokens[reference.to_i][:text]
       else
         reference
       end
@@ -265,9 +277,13 @@ class InvoiceImg < ActiveRecord::Base
   end
 
   def iso_country_from_text(country_txt)
-    fm = FuzzyMatch.new(ISO3166::Country.translations(tagv(:language)))
-    # ["ES", "Espanya"]
-    fm.find(country_txt)[0].downcase rescue invoice.company.country
+    begin
+      fm = FuzzyMatch.new(ISO3166::Country.translations(tagv(:language)))
+      # ["ES", "Espanya"]
+      fm.find(country_txt)[0].downcase
+    rescue
+      invoice.company.country
+    end
   end
 
   def as_json(options)
