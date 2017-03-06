ecc69bcb15978fa02ba084660c257426eb45e22e
Lluís Gili <lluis@ingent.net>
2017-03-06 14:30:46 +0100

    fix error generating invoice drafts, closes #6369

--- app/controllers/invoice_templates_controller.rb
+++ app/controllers/invoice_templates_controller.rb
@@ -73,7 +73,7 @@ class InvoiceTemplatesController < InvoicesController
     @date = Date.today + days.to_i.day
     @drafts = DraftInvoice.includes(:client).
       where("clients.project_id = ?", @project.id).
-      references(:client).order("date ASC")
+      references(:client).order("date ASC").to_a
     templates = InvoiceTemplate.includes(:client).
       where("clients.project_id = ? and date <= ?", @project.id, @date).
       references(:client).order("date ASC")
