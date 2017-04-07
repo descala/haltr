9a52b059b739e1ff26dab8a33616cd5b3414a75d
Lluís Gili <lluis@ingent.net>
2017-04-07 13:46:04 +0200

    sort events

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -17,7 +17,7 @@ class Invoice < ActiveRecord::Base
   TO_UTF_FIELDS = %w(extra_info)
 
   has_many :invoice_lines, -> {order 'position is NULL, position ASC'}, dependent: :destroy
-  has_many :events, -> {order :created_at}
+  has_many :events, -> {order 'created_at DESC'}
   #has_many :taxes, :through => :invoice_lines
   belongs_to :project, :counter_cache => true
   belongs_to :client
