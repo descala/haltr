c0ceb3fe3c22c3b4cf3ed25eebfd4145c82c94ff
Lluís Gili <lluis@ingent.net>
2017-04-07 10:21:36 +0200

    update javascripts

--- assets/javascripts/haltr.js
+++ assets/javascripts/haltr.js
@@ -200,6 +200,18 @@ $(document).ready(function() {
   if ( $(".equalheight").length > 0 ) {
     $('.equalheight').matchHeight();
   }
+  if ( $(".equalheight2").length > 0 ) {
+    $('.equalheight2').matchHeight();
+  }
+  if ( $(".wrapper-dismissible").length > 0 ) {
+    $( ".wrapper-dismissible" ).slideDown( "slow" );
+    $( ".wrapper-dismissible .close" ).click(function() {
+      $( ".wrapper-dismissible" ).slideUp( "fast" );
+    });
+  }
+  if ( $(".modal-launch").length > 0 ) {
+    $('.modal-launch').modal('show');
+  }
 
 });
 
